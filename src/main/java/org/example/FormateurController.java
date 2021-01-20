55347297254fc060651832cd429850ef439d99e0
spacewalker96 <hmiyedhassan@gmail.com>
2021-01-20 14:15:55 +0100

    branch hassan

---
+++ src/main/java/org/example/FormateurController.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeTableColumn;
+
+public class FormateurController {
+
+}
